cf7c39dd45edf26b3acc0d879e4bc430e76a8607
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2022-01-30 18:54:25 -0500

    feat: player info component

--- src/components/PlayerInfo/index.tsx
+++ src/components/PlayerInfo/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { HTMLAttributes } from "react";
 
- import s from './style.module.scss';
+import s from "./style.module.scss";
 
-const PlayerInfo: React.FC = () => {
-  return (
-      <div>
-          
-      </div>
-  );
+interface PlayerInfoProps extends HTMLAttributes<HTMLDivElement> {
+  img: string;
+  name: string;
+  points: string;
 }
+
+const PlayerInfo: React.FC<PlayerInfoProps> = ({ img, name, points, ...rest }) => {
+  return (
+    <div className={s.player} {...rest}>
+      <img src={img} className={s["player__photo"]} alt={name} />
+      <p>{name}</p>
+      <div className={s["player__points"]}>
+        <div className={s.circle}>
+          <span>{points}</span>
+        </div>
+        <p>pts</p>
+      </div>
+    </div>
+  );
+};
 
 export default PlayerInfo;
